4a03a43d7db9bff446611340a7d6919c5f17bd64
Adrian Albu <2412rock@gmail.com>
2024-09-01 22:04:01 +0300

    Friends table

--- init.sql
+++ init.sql
@@ -1,13 +1,15 @@
+-- Check if the database exists, if not, create it
 IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = 'OverflowDB')
 BEGIN
     CREATE DATABASE OverflowDB;
 END;
 GO
--- Switch to the newly created database
-USE master;
-GO
-USE OverflowDB;
 
+-- Switch to the OverflowDB
+USE OverflowDB;
+GO
+
+-- Create Users table if it does not exist
 IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Users')
 BEGIN
     CREATE TABLE Users (
@@ -19,9 +21,18 @@ BEGIN
         NumberOfGames INT
     );
 END;
-
-USE OverflowDB;
 GO
 
-UPDATE Users
-SET Rank = 1000;
+-- Create Friends table if it does not exist
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Friends')
+BEGIN
+    CREATE TABLE Friends (
+        FriendId INT PRIMARY KEY IDENTITY,
+        UserId INT,
+        FriendUserId INT,
+        Status INT CHECK (Status IN (0, 1, 2)), -- Using int values 0, 1, 2 for status
+        FOREIGN KEY (UserId) REFERENCES Users(UserId),
+        FOREIGN KEY (FriendUserId) REFERENCES Users(UserId)
+    );
+END;
+GO
